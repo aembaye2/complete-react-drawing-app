0ad7bb4e724172854e5f09981c8acbb312d2a501
Abel Embaye <aembaye@uark.edu>
2024-11-14 13:29:57 -0600

    Updates

---
+++ precache-manifest.7f91e3e818790f093f28ef4859270bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d44f4c2fd08ca549bf98d2dd00edbcd",
+    "url": "/complete-react-drawing-app/index.html"
+  },
+  {
+    "revision": "a58ca734dcc3d4fd049c",
+    "url": "/complete-react-drawing-app/static/css/main.56005af5.chunk.css"
+  },
+  {
+    "revision": "011451b93742b7d16f7d",
+    "url": "/complete-react-drawing-app/static/js/2.45c8a5b6.chunk.js"
+  },
+  {
+    "revision": "42b10800eb1a84c29b47532fa1a7f8bc",
+    "url": "/complete-react-drawing-app/static/js/2.45c8a5b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58ca734dcc3d4fd049c",
+    "url": "/complete-react-drawing-app/static/js/main.ac00b319.chunk.js"
+  },
+  {
+    "revision": "76c312a446ff5d8107fd",
+    "url": "/complete-react-drawing-app/static/js/runtime-main.0a03778e.js"
+  }
+]);
